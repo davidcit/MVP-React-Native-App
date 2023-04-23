e9f0d772d80c30e6c777a37bf33e9eca7cc14013
unknown <dcitadelle@gmail.com>
2023-04-24 00:39:00 +0200

    add flat list in search js with data films.

--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet,Button, TextInput, View } from "react-native";
+import { StyleSheet,Button, TextInput, View, FlatList,Text } from "react-native";
+import films from "../helpers/filmData";
 class Search extends React.Component{
     render(){
         return(
             <View style={styles.viewStyle}>
                 <TextInput style={styles.inputText} value="Search bar"/>
                 <Button color="#772F1A" title="Rechercher" onPress={ () => {}}/>
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <Text>{item.title}</Text>}
+                />
             </View>
         )
     }
