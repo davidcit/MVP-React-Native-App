8f773a9190c3d8006eebba331d1fb255059176af
unknown <dcitadelle@gmail.com>
2023-04-26 00:27:30 +0200

    Connection to TMDBA API and improved search bar in search.js

--- components/Search.js
+++ components/Search.js
@@ -1,16 +1,40 @@
 import React from "react";
 import { StyleSheet,Button, TextInput, View, FlatList,Text } from "react-native";
 import films from "../helpers/filmData";
+import getFilmsFromApiWithSearchedText  from "../API/TMDBApi";
+import FilmItem from "./FilmItem";
 class Search extends React.Component{
+    /* Privat function ("_") */
+    constructor(props){
+        super(props)
+        this._films = [];
+        this.state = { 
+            films: [],
+            searchedText: '',
+        }
+    }
+    _loadFilms() {
+        console.log(this.state.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
+        if (this.state.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+          getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
+              this.setState({ films: data.results })
+          })
+        }else{
+            alert('Merci d\'inscrire un nom de film');
+        }
+    }
+    _searchTextInputChanged(text) {
+        this.setState({ searchedText: text })
+    }
     render(){
         return(
             <View style={styles.viewStyle}>
-                <TextInput style={styles.inputText} value="Search bar"/>
-                <Button color="#772F1A" title="Rechercher" onPress={ () => {}}/>
+                <TextInput style={styles.inputText} placeholder="Votre recherche" onChangeText={(text) => this._searchTextInputChanged(text)}/>
+                <Button color="#772F1A" title="Rechercher" onPress={ () => this._loadFilms() }/>
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <Text>{item.title}</Text>}
+                    renderItem={({item}) => <FilmItem film={item}/>}
                 />
             </View>
         )
